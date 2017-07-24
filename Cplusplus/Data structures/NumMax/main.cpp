0fc6edade713b1ca8f65069fde57b84112bdd1cc
Ángel Guerrero <_angelguerrero_@outlook.com>
2017-07-23 23:38:03 -0500

    Adding the basic data structures

---
+++ Cplusplus/Data structures/NumMax/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+#define ESPANOL locale::global(locale("spanish"))
+
+//Global variables
+int arrayLength;
+int *numbers;
+
+//Prototype
+int max(int *, int);
+
+int main()
+{
+	ESPANOL;
+
+	cout << "Ingresa el máximo de números a ingresar: ";
+	cin >> arrayLength;
+
+	numbers = new int[arrayLength];
+
+	for (int i = 0; i < arrayLength; i++)
+	{
+		cout << "Ingresa un valor para el índice " << i << ": ";
+		cin >> numbers[i];
+	}
+
+	cout << "\nEl valor máximo de la lista es: " << max(numbers, arrayLength) << "\n" << endl;
+
+	system("pause");
+
+	delete numbers;
+
+	return 0;
+}
+
+int max(int *list, int length)
+{
+	int max = list[0];
+
+	for (int i = 0; i < length; i++)
+	{
+		if (list[i] >= max)
+		{
+			max = list[i];
+		}
+	}
+
+	return max;
+}
---
+++ Cplusplus/Data structures/simplify/main.cpp
@@ -0,0 +1,80 @@
+#include <string>
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+//Global variables
+#define ESPANOL locale::global(locale("spanish"))
+int divider = 2;
+
+// Prototype
+void simplify(double, double);
+bool isInteger(double);
+
+
+int main()
+{
+	double num, den;
+
+	ESPANOL;
+
+	cout << "Ingrese el numerador: ";
+	cin >> num;
+
+	cout << "Ingrese el denominador: ";
+	cin >> den;
+
+	simplify(num, den);
+
+	system("pause");
+
+	return 0;
+}
+
+void simplify(double numerator, double denominator)
+{
+	double r_num;
+	double r_den;
+
+	r_num = numerator / divider;
+	r_den = denominator / divider;
+
+	if (isInteger(r_num) && isInteger(r_den))
+	{
+		cout << "Simplificando: " << numerator << "/" << denominator << endl;
+		simplify(r_num, r_den);
+	}
+	else if (numerator == divider || denominator == divider || r_num <= 1 || r_den <= 1)
+	{
+		cout << "La máxima expresión es: " << numerator << "/" << denominator << endl;
+	}
+	else
+	{
+		divider++;
+		simplify(numerator, denominator);
+	}
+}
+
+bool isInteger(double param)
+{
+	bool val = false;
+
+	double integer, decimal;
+
+	decimal = modf(param, &integer);
+
+	//cout << "decimal: " << decimal << "\nentero: " << integer << endl;
+
+	if (decimal > 0)
+	{
+		//cout << "Es un flotante" << endl;
+		val = false;
+	}
+	else
+	{
+		//cout << "Es un entero" << endl;
+		val = true;
+	}
+
+	return val;
+}
